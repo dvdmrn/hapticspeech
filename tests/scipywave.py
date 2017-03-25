4b42f3c5408d86ef820c7487d0c605a6686548b0
dvdmrn <damarino@cs.ubc.ca>
2017-03-25 00:12:24 -0700

    file cleanup and gitignore

--- tests/scipywave.py
+++
@@ -1,9 +0,0 @@
-import scipy.io.wavfile
-import sounddevice as sd 
-
-filename = "PHRASE_dramatist_female_edited.wav"
-thing = scipy.io.wavfile.read(filename)
-
-for x in thing[1]:
-	sd.play(x, 44100)
-# data, sample_frequency,encoding = wavread(filename)
