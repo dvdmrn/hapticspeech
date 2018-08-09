183e54bcf7847a74dcf1383306eb83da3a37c150
sophychu <chusophy@hotmail.com>
2018-08-08 17:21:32 -0700

    amp filter to make sure vibes are right

--- playback.py
+++ playback.py
@@ -102,7 +102,7 @@ def rms_playback(filepath, offset):
 
             # amp = RMS(subsamples)
 
-            if voiced:
+            if voiced > 200:
                 amp = RMS(subsamples)
             else:
                 amp = 0 
